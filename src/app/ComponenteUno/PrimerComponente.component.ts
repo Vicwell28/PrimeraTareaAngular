766fb4e18f7267c235ac77e6c6c5f125dda43359
Vicwell28 <=>
2022-01-26 13:06:13 -0600

    Pracitca Dos

--- src/app/ComponenteUno/PrimerComponente.component.ts
+++ src/app/ComponenteUno/PrimerComponente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service'
 
 @Component({
     selector: 'HelloWord', 
@@ -6,6 +7,49 @@ import { Component } from '@angular/core';
     styleUrls: ['./PrimerComponente.component.css']
 })
 export class HelloWord{
-    title = 'Welcome to Angular'
-    users = ['Victor', 'Miguel', 'Basurto', 'Juarez']
+    title = 'Welcome to Angular';
+    users = ['Victor', 'Miguel', 'Basurto', 'Juarez'];
+    Activado: boolean = true;
+    name: string = 'Rayan Ray';
+    age: number=0; 
+    address = {
+        street : "H",
+        city : "H"
+    };
+    hobbies = ['',];
+    post : any = []
+
+    constructor(private dataServec : DataService){
+        this.age = 19; 
+        this.address = {
+            street: 'Santiago Ramirez',
+            city: 'Torreón'
+        };
+        this.hobbies  = ['Read','Swimming','Write']
+
+        this.dataServec.getData().subscribe(data => {
+            this.post = data;
+        });
+    }
+
+    seyHello(){
+        alert("Hola");
+    }
+
+    deleteUser(user:any){
+        for(let i = 0; i < this.users.length; i++){
+            if(user == this.users[i]){
+                this.users.splice(i, 1);
+            }
+        }
+    }
+
+    EnviarNombre(idNambe:any){
+        this.users.push(idNambe.value); 
+        idNambe.value=""; 
+        idNambe.focus(); 
+
+        return false; 
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HelloWord } from './ComponenteUno/PrimerComponente.component';
-import { HolaMundoComponent } from './hola-mundo/hola-mundo.component'
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
+import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes : Route[] =[
+  {path: '', component: HelloWord},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWord,
-    HolaMundoComponent
+    HolaMundoComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
